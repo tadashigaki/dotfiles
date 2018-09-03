261c865857842d89aac34334505d0631f339361c
Tadashi Akama <tadashi.akama@gmail.com>
2018-09-03 11:16:42 +0900

    Add yaml-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -127,3 +127,6 @@
 
 (require 'dockerfile-mode)
 (add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.ya?ml$'" . yaml-mode))
