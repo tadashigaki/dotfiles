db3e9fdd10e014d5e19cb0df69253a85e8d059ac
tadashigaki <tadashi.akama@gmail.com>
2020-09-11 23:49:40 +0900

    configure basic emacs settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,9 +5,18 @@
 
 ;;; Commentary:
 
-;;
+;; 
 
 ;;; Code:
+(setq c-basic-offset 4) ; indents 4 chars                                                                                                              
+(setq tab-width 4)          ; 4 char wide for TAB
+(setq indent-tabs-mode nil) ; force use of spaces
+
+(setq inhibit-splash-screen t) ; hide welcome screen
+
+(menu-bar-mode -1) ; hide menu bar
+(tool-bar-mode -1) ; hide tool bar
+(toggle-scroll-bar -1)  ; hide scroll bar
 
 (provide 'init)
 ;;; init.el ends here
