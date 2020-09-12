7d813a2272fc2c98dce224256055ab18702638c2
tadashigaki <tadashi.akama@gmail.com>
2020-09-12 12:09:29 +0900

    add use-package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,25 +52,22 @@
 
 
 ;;;
+;;; use-package
+;;;
+
+(eval-when-compile
+  (require 'use-package))
+
+
+;;;
 ;;; helm
 ;;;
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(solarized-theme helm)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(global-set-key (kbd "M-x") #'helm-M-x)
-(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
-(helm-mode 1)
+(use-package helm
+  :bind (("M-x" . helm-M-x)
+         ("C-x C-c" . helm-M-x)
+         ("C-x C-r" . helm-recentf)))
+
 
 (provide 'init)
 ;;; init.el ends here
