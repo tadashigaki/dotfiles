33b2310975df7158cb749e6302c553ea845ee141
tadashigaki <tadashi.akama@gmail.com>
2020-09-23 22:45:43 +0900

    format

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,39 +9,52 @@
 
 ;;; Code:
 
-;;;
-;;; basic configurations
-;;;
+;; this enables this running method
+;;   emacs -q -l ~/.debug.emacs.d/init.el
+(eval-and-compile
+  (when (or load-file-name byte-compile-current-file)
+    (setq user-emacs-directory
+          (expand-file-name
+           (file-name-directory (or load-file-name byte-compile-current-file))))))
 
-(setq c-basic-offset 4) ; indents 4 chars                                                                                                              
-(setq tab-width 4)          ; 4 char wide for TAB
-(setq indent-tabs-mode nil) ; force use of spaces
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("melpa-stable" . "http://stable.melpa.org/packages/")
+                       ("ELPA" . "http://tromey.com/elpa/")
+                       ("org"   . "https://orgmode.org/elpa/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
 
-(setq inhibit-splash-screen t) ; hide welcome screen
+  (leaf leaf-keywords
+    :ensure t
+    :init
+    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
+    (leaf hydra :ensure t)
+    (leaf el-get :ensure t)
+    (leaf blackout :ensure t)
 
-(menu-bar-mode -1) ; hide menu bar
-(tool-bar-mode -1) ; hide tool bar
-(toggle-scroll-bar -1)  ; hide scroll bar
-(setq initial-scratch-message "") ; hide scratch message 
+    :config
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
 
-(setq auto-save-default nil) ; not make auto safe file
-(setq auto-save-list-file-prefix nil) ; not make auto save list
-(setq create-lockfiles nil) ; not make lock file
-(setq make-backup-files nil) ; not make backup file
+  ;; coustom configuration starts here
 
-(global-display-line-numbers-mode) ; show line numbers
 
-;;;
-;;; theme
-;;;
+;;
+;; theme
+;;
 
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/elpa/")
 (load-theme 'solarized-dark t)
 
-;;;
-;;; package repositories
-;;;
+;;
+;; package repositories
+;;
 
 (require 'package)
   
@@ -54,32 +67,32 @@
   
 (package-initialize)
 
-;;;
-;;; use-package
-;;;
+;;
+;; use-package
+;;
 
 (eval-when-compile
   (require 'use-package))
 
-;;;
-;;; helm
-;;;
+;;
+;; helm
+;;
 
 (use-package helm
   :bind (("M-x" . helm-M-x)
          ("C-x C-c" . helm-M-x)
          ("C-x C-r" . helm-recentf)))
 
-;;;
-;;; magit
-;;;
+;;
+;; magit
+;;
 
 (use-package magit
   :bind ("C-x g" . magit-status))
 
-;;;
-;;; projectile
-;;; 
+;;
+;; projectile
+;;
 
 (use-package projectile
   :ensure t
@@ -130,13 +143,42 @@
  (use-package lsp-mode
  :ensure t))
 
+;;
+;; ivy
+;;
+
+(use-package ivy
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  ;; enable this if you want `swiper' to use it
+  (setq search-default-mode #'char-fold-to-regexp)
+  (global-set-key "\C-s" 'swiper)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  (global-set-key (kbd "<f6>") 'ivy-resume)
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  (global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
+  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+  (global-set-key (kbd "C-c g") 'counsel-git)
+  (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  (global-set-key (kbd "C-c k") 'counsel-ag)
+  (global-set-key (kbd "C-x l") 'counsel-locate)
+  (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(helm-ag flycheck rustic which-key neotree projectile magit use-package lsp-mode rust-mode solarized-theme helm))
+   '(ivy eldoc ## racer quickrun helm-ag flycheck rustic which-key neotree projectile magit use-package lsp-mode rust-mode solarized-theme helm))
  '(projectile-mode t nil (projectile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -146,4 +188,28 @@
  )
 
 (provide 'init)
+
+;; Local Variables:
+
+;; indent-tabs-mode: nil
+(setq c-basic-offset 4) ; indents 4 chars
+(setq tab-width 4)          ; 4 char wide for TAB
+(setq indent-tabs-mode nil) ; force use of spaces
+
+(setq inhibit-splash-screen t) ; hide welcome screen
+
+(menu-bar-mode -1) ; hide menu bar
+(tool-bar-mode -1) ; hide tool bar
+(toggle-scroll-bar -1)  ; hide scroll bar
+(setq initial-scratch-message "") ; hide scratch message
+
+(setq auto-save-default nil) ; not make auto safe file
+(setq auto-save-list-file-prefix nil) ; not make auto save list
+(setq create-lockfiles nil) ; not make lock file
+(setq make-backup-files nil) ; not make backup file
+
+(global-display-line-numbers-mode) ; show line numbers
+
+;; End:
+
 ;;; init.el ends here
