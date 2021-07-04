2750e06553b6644e95b5a2a0d12b1d0108f71fe0
tadashigaki <tadashi.akama@gmail.com>
2021-07-04 22:13:07 +0900

    pruned

--- setup-mac.sh
+++ setup-mac.sh
@@ -10,31 +10,24 @@ brew install wget
 brew install git
 brew install hub
 brew install jq
-brew install go
 brew install telnet
 brew install nodebrew
-brew install tig
 brew uninstall grep
 brew install grep
 brew install kubectl
 brew install tfenv
-brew install fish
 brew install gibo
 brew install ngrok
-brew install minamijoyo/tfschema/tfschema
 brew install zsh-completions
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
 brew install mysql-client
 brew install anyenv
 
-brew tap caskroom/cask
 brew cask install emacs
 brew cask install google-chrome
-brew cask install google-hangouts
 brew cask install docker
 brew cask install spotify
-brew cask install minikube
 
 curl https://sdk.cloud.google.com | bash
 exec -l $SHELL
